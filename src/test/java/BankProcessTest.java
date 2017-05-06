fb785cef7761e65248afa7f42a7836077f5492d4
Rama Lanka <rama.lanka@standardbank.co.za>
2017-05-06 13:29:59 +0200

    First Commit

---
+++ src/test/java/BankProcessTest.java
@@ -0,0 +1,14 @@
+import junit.framework.TestCase;
+
+
+public class BankProcessTest extends TestCase {
+
+	public void testTransferFunds() {
+		//fail("Not yet implemented");
+	}
+
+	public void testGetBalanceOf() {
+		//fail("Not yet implemented");
+	}
+
+}
